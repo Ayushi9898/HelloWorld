73e5aa79f8fea279c06b9fc26b7e90ea6ee365f8
Ayushi@9898 <96721856+Ayushi9898@users.noreply.github.com>
2022-03-20 00:14:56 +0530

    Add files via upload
    helloWorld program in cpp :)

---
+++ AyushiGautam.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void h(){
+    cout<<"H";
+}
+void e(){
+    cout<<"E";
+}
+void l(){
+    cout<<"L";
+}
+void o(){
+    cout<<"O";
+}
+void w(){
+    cout<<"W";
+}
+void r(){
+    cout<<"R";
+}
+void d(){
+    cout<<"D";
+}
+void s(){
+    cout<<" ";
+}
+void m(){
+    cout<<endl;
+}
+int main(){
+    h();e();l();l();o();s();w();o();r();l();d();m();
+}
